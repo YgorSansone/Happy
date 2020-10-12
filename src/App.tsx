b492796068116acae0316f99867232a81a23df06
Ygor Sansone <ygorsansone@gmail.com>
2020-10-12 16:39:00 -0300

    primeira tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './styles/global.css';
+import './styles/pages/landing.css';
+import logoImg from './images/Logo.svg';
+import {FiArrowRight} from 'react-icons/fi';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div id="page-landing">
+      <div className="content-wrapprer">
+        <img src={logoImg} alt="Happy" />
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>São Paulo</strong>
+          <span>Taubaté</span>
+        </div>
+        
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
         </a>
-      </header>
+      </div>
     </div>
   );
 }
