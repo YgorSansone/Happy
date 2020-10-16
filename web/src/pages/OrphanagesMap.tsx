136041526804b1729934bb19d10e61277b805cfa
Ygor Sansone <ygorsansone@gmail.com>
2020-10-15 21:32:21 -0300

    Finalizando paginas de mapa

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css'
-import {Map, TileLayer} from 'react-leaflet';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import Leaflet from 'leaflet';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170,2]
+})
 function OrphanagesMap() {
     return (
         <div id="page-map">
@@ -26,8 +34,21 @@ function OrphanagesMap() {
             >
                 {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
                 <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+                
+                <Marker
+                icon={(mapIcon)}
+                 position={[-23.0133652,-45.541358]}
+                >
+                 <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                     Lar das meninas
+                     <Link to="/orphanages/1">
+                         <FiArrowRight size={20} color="#FFF"/>
+                     </Link>
+                 </Popup>
+                 </Marker>
+                 
             </Map>
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
         </div>
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import OrphanagesMap from "./pages/OrphanagesMap";
 import Landing from "./pages/Landing";
+import Orphanage from "./pages/Orphanage";
+import CreateOrphanage from "./pages/CreateOrphanage";
 function Routes() {
     return (
         <BrowserRouter>
             <switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/app" component={OrphanagesMap} />
+
+                <Route path="/orphanages/create" component={CreateOrphanage} />
+                <Route path="/orphanages/:id" component={Orphanage} />
             </switch>
         </BrowserRouter>
     );
